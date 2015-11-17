c2ca0f84adac5e38e907a5ef98e40a136c3b2750
Chris Stook <chris@chrisstook.com>
2015-11-17 06:37:03 -0500

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- src/ps3000_open_unit.jl
+++ src/ps3000_open_unit.jl
@@ -3,7 +3,7 @@
 export ps3000_open_unit
 
 function ps3000_open_unit()
-  ps_status = ccall((:ps3000_close_unit, ps3000driver),Int16,(),) 
+  ps_status = ccall((:ps3000_open_unit, ps3000driver),Int16,(),) 
   if ps_status == -1
     error("ps3000 Error: Failed To Open")
   end
