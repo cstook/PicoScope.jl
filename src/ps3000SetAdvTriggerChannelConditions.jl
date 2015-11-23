bfe493e7c864abac1e4f3be9db23b6b5176e69ba
Chris Stook <chris@chrisstook.com>
2015-11-23 01:52:34 -0500

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- src/ps3000SetAdvTriggerChannelConditions.jl
+++ src/ps3000SetAdvTriggerChannelConditions.jl
@@ -27,3 +27,15 @@ function ps3000SetAdvTriggerChannelConditions(handle     :: Int16,
   end
   return nothing
 end
+
+function ps3000SetAdvTriggerChannelConditions(handle     :: Int16)
+  # call with no conditions to turn trigger off
+  nconditions = 0
+  ps_status = ccall(:ps3000SetAdvTriggerChannelConditions, ps3000driver), Int16,
+  (Int16, Ptr{Void}, Int16),
+  handle, Ptr{Void}(C_NULL), nconditions)
+  if ps_status == 0
+    error("ps3000 Error: one of the parameters is out of range")
+  end
+  return nothing
+end
